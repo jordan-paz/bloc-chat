dc0e25ac1b16bf9c3173ba6ab9ce67a1858b9e9c
Jordan Paz <jordandavidpaz@gmail.com>
2018-10-15 12:06:13 -0700

    finished the list-messages component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import RoomList from './components/RoomList.js'
+import MessageList from './components/MessageList'
 import * as firebase from 'firebase';
 
   // Initialize Firebase
@@ -14,9 +15,25 @@ import * as firebase from 'firebase';
   firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      activeRoom: ''
+    }
+    this.setActiveRoom = this.setActiveRoom.bind(this)
+  }
+
+  setActiveRoom(room) {
+    this.setState({ activeRoom: room })
+    console.log("setActiveRoom fired:" + room.name)
+  }
+
   render() {
     return(
-      <RoomList firebase={firebase} />
+      <div className="app-component">
+        <RoomList firebase={firebase} activeRoom={this.state.activeRoom} setActiveRoom={this.setActiveRoom.bind(this)} />
+        <MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
+      </div>
     )
   }
 }
